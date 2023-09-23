c1e3b6bf73fc597536ffc90358ef5108d28635fb
Richard E. Hoehn <rhoehn@hamilton.cs.mtsu.edu>
2023-09-22 22:39:20 -0500

    ResID-Done

---
+++ OL2/cifar10-resid.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import time
+start = time.time() # Start Timing
+
+
+import numpy as np
+import torch
+import torchvision
+import torchmetrics
+import lightning.pytorch as pl
+from torchinfo import summary 
+import pandas as pd
+
+if (torch.cuda.is_available()):
+    device = ("cuda")
+else:
+    device = ("cpu")
+    
+print(f"Use GPU: {torch.cuda.is_available()}")
+
+# Config Section
+cfg_data_folder = "datasets/cifar10"
+cfg_max_epochs  = 50;
+cfg_num_workers = 2
+cfg_hidden_dims = 35
+cfg_num_hidden_layers = 18
+
+cfg_logger_dir="logs"
+cfg_logger_name="OL2"
+cfg_logger_version="resid"
+
+# Load the data set and scale to [-1,+1]
+training_dataset = torchvision.datasets.CIFAR10(root=cfg_data_folder, download=True, train=True)
+ 
+x_train = (torch.Tensor(training_dataset.data) / 127.5) - 1.0
+y_train = torch.Tensor(training_dataset.targets).to(torch.long)
+
+# You can set a seed value here if you
+# want to control the shuffling process...
+rng = np.random.default_rng()
+permutation = rng.permutation(x_train.shape[0]) 
+split_point = int(x_train.shape[0] * 0.8) # 80% / 20% split
+
+# Split into validation/training - keep test # set aside for later...
+x_val = x_train[permutation][split_point:]
+y_val = y_train[permutation][split_point:]
+x_train = x_train[permutation][:split_point]
+y_train = y_train[permutation][:split_point]
+
+# Define residual layer
+class ResidualLayer(torch.nn.Module): 
+    def __init__(self, hidden_size, **kwargs):
+        super().__init__(**kwargs)
+        self.residual = torch.nn.Linear(hidden_size, hidden_size)
+        self.activation = torch.nn.ReLU()
+
+    def forward(self, x):
+        y = x
+        y = self.activation(self.residual(y))
+        y = y + x # Add residual
+        return y
+
+class ResidualNetwork(torch.nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, num_hidden_layers, **kwargs):
+        super().__init__(**kwargs)
+        self.flatten_layer = torch.nn.Flatten()
+        
+        # Initial linear projection
+        layers = [torch.nn.Linear(input_size.numel(), hidden_size),
+                  torch.nn.ReLU(),
+                  torch.nn.BatchNorm1d(hidden_size),
+                  torch.nn.Dropout(0.5)
+                 ]
+        for _ in range(num_hidden_layers):
+            layers.append(ResidualLayer(hidden_size))
+            layers.append(torch.nn.ReLU())  # Use ReLU activation
+            layers.append(torch.nn.BatchNorm1d(hidden_size))  # Add Layer Normalization
+
+        self.hidden_layers = torch.nn.Sequential(*layers)
+        self.output_layer = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        y = x
+        y = self.flatten_layer(y)
+        y = self.hidden_layers(y)
+        y = self.output_layer(y)
+        return y
+        
+    def predict(self, x):
+        y = x
+        y = self.forward(y)
+        y = torch.softmax(y, -1)
+        return y
+
+# Define Trainable Module
+class PLModel(pl.LightningModule):
+    def __init__(self, module, **kwargs):
+        # This is the contructor, where we typically make
+        # layer objects using provided arguments. 
+        super().__init__(**kwargs) # Call the super class constructor 
+        self.module = module
+        
+        # This creates an accuracy function
+        self.model_acc = torchmetrics.classification.Accuracy(task='multiclass', 
+                                                              num_classes=module.output_layer.out_features)
+        # This creates a loss function
+        self.model_loss = torch.nn.CrossEntropyLoss()
+    
+    def forward(self, x):
+        return self.module.forward(x) 
+        
+    def predict(self, x):
+        return self.module.predict(x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=1e-4)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx): 
+        x, y_true = train_batch
+        y_pred = self(x)
+        acc = self.model_acc(y_pred, y_true)
+        loss = self.model_loss(y_pred, y_true) 
+        self.log('train_acc',  acc,  on_step=False, on_epoch=True)
+        self.log('train_loss', loss, on_step=False, on_epoch=True) 
+        return loss
+
+
+    def validation_step(self, val_batch, batch_idx): 
+        x, y_true = val_batch
+        y_pred = self(x)
+        acc = self.model_acc(y_pred, y_true)
+        loss = self.model_loss(y_pred, y_true)
+        self.log('val_acc',  acc,  on_step=False, on_epoch=True)
+        self.log('val_loss', loss, on_step=False, on_epoch=True)
+        return loss
+        
+    def test_step(self, test_batch, batch_idx):
+        x, y_true = test_batch
+        y_pred = self(x)
+        acc = self.model_acc(y_pred,y_true)
+        loss = self.model_loss(y_pred,y_true)
+        self.log('test_acc',  acc,  on_step=False, on_epoch=True)
+        self.log('test_loss', loss, on_step=False, on_epoch=True)
+        return loss
+
+model = PLModel(ResidualNetwork(x_train.shape[1:],
+                                len(y_train.unique()),
+                                cfg_hidden_dims,
+                                cfg_num_hidden_layers))
+
+summary(model, input_size=(1,)+x_train.shape[1:])
+
+xy_train = torch.utils.data.DataLoader(list(zip(x_train, y_train)),
+                                       shuffle=True,
+                                       batch_size=250,
+                                       num_workers=cfg_num_workers) # Using CFG
+
+xy_val = torch.utils.data.DataLoader(list(zip(x_val, y_val)),
+                                     shuffle=False,
+                                     batch_size=250,
+                                     num_workers=cfg_num_workers) # Using CFG
+
+
+# Setup Logger
+logger = pl.loggers.CSVLogger(cfg_logger_dir, 
+                              name=cfg_logger_name, 
+                              version=cfg_logger_version)
+
+trainer = pl.Trainer(logger=logger,
+                     max_epochs=cfg_max_epochs,
+                     enable_progress_bar=False,
+                     log_every_n_steps=0)
+
+# Train Model -  This takes awhile!!!
+trainer.fit(model, xy_train, xy_val)
+
+results = pd.read_csv(f"{logger.log_dir}/metrics.csv")
+
+# Hanlding Timing
+end = time.time()
+elapsed = end - start
+
+print("")
+print(f"Processing Time: {elapsed:.6f} seconds\n")
+print("Validation accuracy:",*["%.8f"%(x) for x in results['val_acc'][np.logical_not(np.isnan(results["val_acc"]))]])
+print("")
+print("")
